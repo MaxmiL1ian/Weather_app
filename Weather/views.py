e4c1feb1b310d1a1d7a2cd9320ce72b231d0a42a
MaxmiL1ian <bezborodovmado2016@gmail.com>
2024-04-23 23:13:08 +0800

    request

--- Weather/views.py
+++ Weather/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
 
 def index(request):
-    return render(request,'Weather/index.html')
+    API_KEY = os.getenv('API_KEY')
+    API_URL = os.getenv('API_URL')
+
+    res = requests.get(API_URL.format("London",API_KEY)).json()
+
+    city_info = {
+        'city':'London',
+        'temp': '25',
+        'wind': '15'
+    }
+
+    context = {
+        'info':city_info
+    }
+    return render(request,'Weather/index.html',context)
