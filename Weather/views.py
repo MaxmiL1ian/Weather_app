2b1c43d3c765e44c80fc8f0cccb8e8a18b54915a
MaxmiL1ian <bezborodovmado2016@gmail.com>
2024-04-24 16:57:40 +0800

    forms

--- Weather/views.py
+++ Weather/views.py
@@ -1,5 +1,7 @@
+
 from django.shortcuts import render
 from .models import City
+from .forms import CityForm
 import os
 from dotenv import load_dotenv
 import requests
@@ -11,8 +13,15 @@ def index(request):
     API_KEY = os.getenv('API_KEY')
     API_URL = os.getenv('API_URL')
 
-    all_cities = []
+    if (request.method == 'POST'):
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
+
     cities = City.objects.all()
+
+    all_cities = []
 
     for city in cities:
         res = requests.get(API_URL.format(city.name,API_KEY)).json()
@@ -26,6 +35,7 @@ def index(request):
         all_cities.append(city_info)
 
     context = {
-        'all_info':all_cities
+        'all_info':all_cities,
+        'form':form
     }
     return render(request,'Weather/index.html',context)
